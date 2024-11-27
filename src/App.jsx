94acd51014ff7e95b67037e0bdce35a334e9ed53
AndreaPotenza <andreapoten3@gmail.com>
2024-11-27 17:03:19 +0100

    completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Header from './components/header'
+import Card from './components/post/Card/card'
+import Footer from './components/footer'
 
 function App() {
 
   return (
     <>
       <Header />
+      <main>
+        <div className='container'>
+          <Card />
+        </div>
+      </main>
+      <Footer />
     </>
   )
 }
--- src/components/post/Card/card.jsx
+++ src/components/post/Card/card.jsx
@@ -1,20 +1,23 @@
+import style from './Card.module.css'
+import Button from './../../buttons/button'
+
 function Card() {
     return (
-        <div>
-            <figure>
-                <img src="" alt="" />
+        <div className={style.card}>
+            <figure className={style.figure}>
+                <img className={style.image} src="../../../public/Uitachi5.png" alt="" />
             </figure>
-            <div>
+            <div className={style.body}>
                 <h3>
                     Titolo del Post
                 </h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nihil non tempora dignissimos autem odit labore illum rem! Vel, saepe, similique voluptas placeat temporibus rerum quas dolorem eos voluptatibus impedit harum animi laboriosam vitae iste.
+                <p className={style.description}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ipsum ducimus necessitatibus esse placeat tempore facilis nam repellat ea. Voluptate quod id voluptatum. Omnis nisi voluptatem distinctio odio aliquam quas. Voluptatum ipsa deleniti dignissimos commodi! Cumque esse perferendis eaque quod?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, delectus.
                 </p>
-                <button>
-                    leggi di più
-                </button>
+                <Button />
             </div>
         </div>
     )
 }
+
+export default Card
