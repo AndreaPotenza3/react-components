e49aa578c26dd53c6452d78350797e599e97301b
AndreaPotenza <andreapoten3@gmail.com>
2024-11-27 15:52:13 +0100

    recommit

---
+++ src/components/post/Card/card.jsx
@@ -0,0 +1,20 @@
+function Card() {
+    return (
+        <div>
+            <figure>
+                <img src="" alt="" />
+            </figure>
+            <div>
+                <h3>
+                    Titolo del Post
+                </h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus nihil non tempora dignissimos autem odit labore illum rem! Vel, saepe, similique voluptas placeat temporibus rerum quas dolorem eos voluptatibus impedit harum animi laboriosam vitae iste.
+                </p>
+                <button>
+                    leggi di più
+                </button>
+            </div>
+        </div>
+    )
+}
